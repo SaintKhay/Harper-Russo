92be6c335a3ad548ec2e21097db4c2a1bc05082d
Adesida Korede <adesidakorede2019@gmail.com>
2025-06-02 18:47:21 +0100

    Added some functionality to the faq QA  section

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-"use strict";
+'use strict';
+// const displayFaqAnswer = document.querySelectorAll('.questions-container');
+
+// displayFaqAnswer.addEventListener('click', () => {
+//   const faqAnswer = document.querySelector('.faq-answer');
+//   faqAnswer.forEach(ans => ans.classList.toggle('hidden'));
+// });
+
+const displayFaqAnswer = document.querySelectorAll('.questions-container');
+
+displayFaqAnswer.forEach(container => {
+  container.addEventListener('click', () => {
+    const answer = container.querySelector('.faq-answer');
+
+    const openIcon = container.querySelector('.open-icon');
+    const closeIcon = container.querySelector('.close-icon');
+    answer.classList.toggle('hidden');
+    openIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+  });
+});
