851a52d63b2b232c83da1f75b08975e774770089
Adesida Korede <adesidakorede2019@gmail.com>
2025-06-19 07:21:16 +0100

    Added Page Navigation and Smooth scrolling

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 'use strict';
-// const displayFaqAnswer = document.querySelectorAll('.questions-container');
-
-// displayFaqAnswer.addEventListener('click', () => {
-//   const faqAnswer = document.querySelector('.faq-answer');
-//   faqAnswer.forEach(ans => ans.classList.toggle('hidden'));
-// });
 
 const displayFaqAnswer = document.querySelectorAll('.questions-container');
+const btnScroll = document.querySelector('.header-btn');
+const sectionArticle = document.querySelector('.article-section');
+const link = document.querySelectorAll('.links');
 
 displayFaqAnswer.forEach(container => {
   container.addEventListener('click', () => {
     const answer = container.querySelector('.faq-answer');
-
     const openIcon = container.querySelector('.open-icon');
     const closeIcon = container.querySelector('.close-icon');
     answer.classList.toggle('hidden');
@@ -19,3 +15,23 @@ displayFaqAnswer.forEach(container => {
     closeIcon.classList.toggle('hidden');
   });
 });
+
+btnScroll.addEventListener('click', function (e) {
+  e.preventDefault();
+  sectionArticle.scrollIntoView({ behavior: 'smooth' });
+
+  console.log(sectionArticle.getBoundingClientRect());
+  console.log(btnScroll.getBoundingClientRect());
+});
+
+//Page Navigation
+
+document.querySelectorAll('.links').forEach(element =>
+  element.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    console.log(id);
+
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  })
+);
