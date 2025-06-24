5e28c769dace18838df8e3cd096fd6865217ec33
Adesida Korede <adesidakorede2019@gmail.com>
2025-06-24 10:02:19 +0100

    Implemented a Sticky Navigation and updated the testimonial section

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 'use strict';
 
+const header = document.querySelector('header');
+const nav = document.querySelector('nav');
 const displayFaqAnswer = document.querySelectorAll('.questions-container');
 const btnScroll = document.querySelector('.header-btn');
 const sectionArticle = document.querySelector('.article-section');
 const link = document.querySelectorAll('.links');
-
-displayFaqAnswer.forEach(container => {
-  container.addEventListener('click', () => {
-    const answer = container.querySelector('.faq-answer');
-    const openIcon = container.querySelector('.open-icon');
-    const closeIcon = container.querySelector('.close-icon');
-    answer.classList.toggle('hidden');
-    openIcon.classList.toggle('hidden');
-    closeIcon.classList.toggle('hidden');
-  });
-});
+const testimonial = document.querySelector('.testimonial-section');
+const carousel = [...document.querySelectorAll('.carousel')];
 
 btnScroll.addEventListener('click', function (e) {
   e.preventDefault();
@@ -26,12 +19,87 @@ btnScroll.addEventListener('click', function (e) {
 
 //Page Navigation
 
-document.querySelectorAll('.links').forEach(element =>
-  element.addEventListener('click', function (e) {
-    e.preventDefault();
-    const id = this.getAttribute('href');
+document.querySelector('.nav-links').addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(e.target);
+
+  if (e.target.classList.contains('links')) {
+    const id = e.target.getAttribute('href');
     console.log(id);
 
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  })
-);
+  }
+});
+
+//Sticky Navigation
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    nav.classList.add('nav-sticky');
+  } else {
+    nav.classList.remove('nav-sticky');
+  }
+};
+
+const hearderObs = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  // rootMargin: '90px',
+});
+hearderObs.observe(header);
+
+//Faq questions Toggle
+
+document.querySelector('.faq-all').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const container = e.target.closest('.questions-container');
+  console.log(container);
+  if (!container) return;
+
+  if (e.target.classList.contains('questions-container')) {
+    const answer = container.querySelector('.faq-answer');
+    const openIcon = container.querySelector('.open-icon');
+    const closeIcon = container.querySelector('.close-icon');
+
+    console.log(answer.classList);
+    answer.classList.toggle('hidden');
+    openIcon.classList.toggle('hidden');
+    closeIcon.classList.toggle('hidden');
+  }
+});
+
+// //Testimonial scroll
+let currentIndex = 0;
+
+//Initially show the first testimonial
+carousel[currentIndex].classList.add('carousel-active');
+
+const dots = document.querySelectorAll('.dot');
+//Events
+
+testimonial.addEventListener('click', function (e) {
+  e.preventDefault();
+  const clicked = e.target.closest('.btn__testimonial');
+  if (!clicked) return;
+
+  //Remove active class from current
+  carousel.forEach(c => c.classList.remove('carousel-active'));
+  dots.forEach(d => d.classList.remove('dot--fill'));
+
+  //Checks if the correct button was clicked and displays the next testimonial
+  if (clicked.classList.contains('btn__testimonial')) {
+    if (clicked.classList.contains('btn--left')) {
+      currentIndex--;
+      if (currentIndex < 0) currentIndex = carousel.length - 1;
+    } else currentIndex++;
+
+    if (currentIndex < carousel.length) {
+    } else {
+      currentIndex = 0;
+    }
+    dots[currentIndex].classList.add('dot--fill');
+    carousel[currentIndex].classList.add('carousel-active');
+  }
+});
