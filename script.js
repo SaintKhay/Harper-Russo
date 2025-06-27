7c494f3ae7933d3ea0b96d657f62cd1bdc720f7e
Adesida Korede <adesidakorede2019@gmail.com>
2025-06-27 10:40:26 +0100

    Testimonial functionality and animation

--- script.js
+++ script.js
@@ -13,41 +13,66 @@ btnScroll.addEventListener('click', function (e) {
   e.preventDefault();
   sectionArticle.scrollIntoView({ behavior: 'smooth' });
 
-  console.log(sectionArticle.getBoundingClientRect());
-  console.log(btnScroll.getBoundingClientRect());
+  // console.log(sectionArticle.getBoundingClientRect());
+  // console.log(btnScroll.getBoundingClientRect());
 });
 
 //Page Navigation
 
 document.querySelector('.nav-links').addEventListener('click', function (e) {
   e.preventDefault();
-  console.log(e.target);
 
   if (e.target.classList.contains('links')) {
     const id = e.target.getAttribute('href');
-    console.log(id);
 
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
 
 //Sticky Navigation
+
+const navHeight = nav.getBoundingClientRect().height;
 const stickyNav = function (entries) {
   const [entry] = entries;
 
   if (!entry.isIntersecting) {
     nav.classList.add('nav-sticky');
+    document.body.style.paddingTop = `-${navHeight}px`;
   } else {
     nav.classList.remove('nav-sticky');
+    document.body.style.paddingTop = `0px`;
   }
 };
 
 const hearderObs = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  // rootMargin: '90px',
+  rootMargin: `-${navHeight}px`,
 });
 hearderObs.observe(header);
+
+//Reveal Section
+const sectionAll = document.querySelectorAll('.section');
+
+const sectionObserver = function (entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+
+    observer.unobserve(entry.target);
+  });
+};
+
+const revealSection = new IntersectionObserver(sectionObserver, {
+  root: null,
+  threshold: 0.15,
+});
+
+sectionAll.forEach(el => {
+  revealSection.observe(el);
+  el.classList.add('section--hidden');
+});
 
 //Faq questions Toggle
 
@@ -55,51 +80,103 @@ document.querySelector('.faq-all').addEventListener('click', function (e) {
   e.preventDefault();
 
   const container = e.target.closest('.questions-container');
-  console.log(container);
-  if (!container) return;
 
-  if (e.target.classList.contains('questions-container')) {
+  if (!container) return;
+  console.log(e.target);
+
+  if (container.classList.contains('questions-container')) {
     const answer = container.querySelector('.faq-answer');
     const openIcon = container.querySelector('.open-icon');
     const closeIcon = container.querySelector('.close-icon');
 
-    console.log(answer.classList);
     answer.classList.toggle('hidden');
     openIcon.classList.toggle('hidden');
     closeIcon.classList.toggle('hidden');
   }
 });
 
-// //Testimonial scroll
-let currentIndex = 0;
+//Slider
 
-//Initially show the first testimonial
-carousel[currentIndex].classList.add('carousel-active');
+const slide = function () {
+  const carouselSlides = document.querySelectorAll('.slide');
+  const slider = document.querySelector('.slider');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
 
-const dots = document.querySelectorAll('.dot');
-//Events
+  let curSlide = 0;
+  const maxSlide = carouselSlides.length;
 
-testimonial.addEventListener('click', function (e) {
-  e.preventDefault();
-  const clicked = e.target.closest('.btn__testimonial');
-  if (!clicked) return;
+  //Functions
+  const createDots = function () {
+    carouselSlides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}" ></button>`
+      );
+    });
+  };
 
-  //Remove active class from current
-  carousel.forEach(c => c.classList.remove('carousel-active'));
-  dots.forEach(d => d.classList.remove('dot--fill'));
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll('.dots__dot')
+      .forEach(dot => dot.classList.remove('dots__dot--active'));
 
-  //Checks if the correct button was clicked and displays the next testimonial
-  if (clicked.classList.contains('btn__testimonial')) {
-    if (clicked.classList.contains('btn--left')) {
-      currentIndex--;
-      if (currentIndex < 0) currentIndex = carousel.length - 1;
-    } else currentIndex++;
+    document
+      .querySelector(`.dots__dot[data-slide='${slide}']`)
+      .classList.add('dots__dot--active');
+  };
 
-    if (currentIndex < carousel.length) {
+  const goToSlides = function (slide) {
+    carouselSlides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
     } else {
-      currentIndex = 0;
+      curSlide++;
     }
-    dots[currentIndex].classList.add('dot--fill');
-    carousel[currentIndex].classList.add('carousel-active');
-  }
-});
+
+    goToSlides(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+
+    goToSlides(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    createDots();
+    goToSlides(0);
+    activateDot(0);
+  };
+  init();
+
+  //Event handlers
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'ArrowLeft') prevSlide();
+    e.key === 'ArrowRight' && nextSlide();
+  });
+
+  dotContainer.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dots__dot')) {
+      curSlide = Number(e.target.dataset.slide);
+      goToSlides(curSlide);
+      activateDot(curSlide);
+    }
+  });
+};
+slide();
